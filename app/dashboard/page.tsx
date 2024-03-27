afca98914e083827af07e0a1ef2425b9e30d1511
matheus <matheusbf.mf@gmail.com>
2024-03-27 00:29:17 -0300

    tela login

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React, {useState} from "react";
+
+import { useRouter } from 'next/navigation'
+export default function Dashboard() {
+
+    return (
+        <div>
+            <h1>Dash</h1>
+        </div>
+    )
+}
